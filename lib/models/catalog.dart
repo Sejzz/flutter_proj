4da5ba48304c781a5aa7f3607f7ad5a068012e6b
Sejal Kothari <kotharisejal143@gmail.com>
2022-01-19 17:41:25 +0530

    day9 completed

---
+++ lib/models/catalog.dart
@@ -0,0 +1,22 @@
+class Item{
+
+  final String id;
+  final String name;
+  final String desc;
+  final num price;
+  final String color;
+  final String image;
+
+  Item({required this.id, required this.name, required this.desc, required this.price, required this.color, required this.image});
+}
+
+final products = [
+  Item(
+  id: "c001", 
+  name: "iPhone 12 Pro", 
+  desc: "Apple iPhone 12th generation", 
+  price: 999, 
+  color: "33505a", 
+  image: "image")
+    
+];
